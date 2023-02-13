1fd9bad730159b56818f10bd8ffa9f5c1e96a7a6
alfomin11 <fominal11@mail.ru>
2023-02-13 20:11:18 +0300

    feat(paginator): add paginator for home and group pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from .models import Post, Group
 from .forms import PostForm
 
@@ -10,13 +11,16 @@ def index(request):
     Функция для представления главной страницы.
 
     :param request:
-    :return: шаблон главной страницы, в который объединяет 10 свежих по дате публикации постов в один общий текст
+    :return: шаблон главной страницы, содержащий все посты
     """
-    # select всех записей модели Post, сортируем по дате (знак "-" означает DESC, сначала большие даты, т.е.
-    # самые свежие) и выводим топ10
-    latest = Post.objects.all()[:11]
+
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)  # показывать по 10 записей на странице.
+    page_number = request.GET.get('page')  # переменная в URL с номером запрошенной страницы
+    page = paginator.get_page(page_number)  # получить записи с нужным смещением
+
     template = "index.html"
-    context = {"posts": latest}
+    context = {'page': page, 'paginator': paginator}
 
     return render(
         request,  # первый аргумент всегда request
@@ -32,19 +36,20 @@ def group_posts(request, slug):
     :param request:
     :param slug: название группы
 
-    :return: шаблон страницы группы, в который вставляет топ12 свежих по дате публикации постов конкретно заданной группы
+    :return: шаблон страницы группы, содержащий посты только заданной группы
     """
 
     # функция get_object_or_404 получает по заданным критериям объект из базы данных или возвращает сообщение об ошибке,
     # если объект не найден
     group = get_object_or_404(Group, slug=slug)
 
-    # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления условия WHERE group_id = {group_id}
-    # posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    posts = group.posts.all()[:12]  # используем related_name модели Group
+    post_list = group.posts.all()  # используем related_name модели Group
+    paginator = Paginator(post_list, 10)  # показывать по 10 записей на странице.
+    page_number = request.GET.get('page')  # переменная в URL с номером запрошенной страницы
+    page = paginator.get_page(page_number)  # получить записи с нужным смещением
 
     template = "group.html"
-    context = {"group": group, "posts": posts}
+    context = {"group": group, 'page': page, 'paginator': paginator}
 
     return render(request, template, context)
 
